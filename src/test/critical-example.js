ce0833e86873bc2ff917ac49e3cc095a40bb79d3
uncazzy <azurda@icloud.com>
2025-01-17 20:47:12 -0500

    .

--- src/test/critical-example.js
+++ src/test/critical-example.js
@@ -1,19 +1,3 @@
-function queryDatabase(userInput) {
-  const query = `SELECT * FROM users WHERE id = ${userInput}`;
-  return executeQuery(query);
-}
-
-const eventHandlers = [];
-function addEventHandler() {
-  const handler = () => console.log('event');
-  document.addEventListener('click', handler);
-  eventHandlers.push(handler);
-}
-
-function processUserData(data) {
-  return data.user.profile.settings;
-}
-
 function processArray(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < 0) {
